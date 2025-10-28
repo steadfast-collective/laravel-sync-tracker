8d22b22625311aa0ba83173afb70e18fca220483
Sami Walbury <sami@steadfastcollective.com>
2025-10-28 17:09:35 +0000

    Added docblocks to SyncTrackedEntity for better autocomplete

--- src/Models/SyncTrackedEntity.php
+++ src/Models/SyncTrackedEntity.php
@@ -5,6 +5,37 @@ namespace WizardingCode\FlowNetwork\SyncTracker\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * @property int $id
+ * @property string $trackable_type
+ * @property int $trackable_id
+ * @property string|null $external_id
+ * @property string|null $log_level
+ * @property string|null $source
+ * @property \Illuminate\Support\Carbon|null $synced_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property array<array-key, mixed>|null $metadata
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity where()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereTrackabletype($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereTrackableid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereExternalid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereLoglevel($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereSource($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereSyncedat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereCreatedat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereUpdatedat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereDeletedat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SyncTrackedEntity whereMetadata($value)
+ *
+ * @mixin \Eloquent
+ */
 class SyncTrackedEntity extends Model
 {
     /**
