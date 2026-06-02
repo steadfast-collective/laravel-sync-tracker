37f275f70ecfd89ee335ea63efd2a82b89bb8a7a
Sami Walbury <sami@steadfastcollective.com>
2026-06-02 16:15:11 +0100

    chore: add PHPStan static analysis

--- tests/Models/TestModel.php
+++ tests/Models/TestModel.php
@@ -5,6 +5,10 @@ namespace WizardingCode\FlowNetwork\SyncTracker\Tests\Models;
 use Illuminate\Database\Eloquent\Model;
 use WizardingCode\FlowNetwork\SyncTracker\Traits\HasSyncTracking;
 
+/**
+ * @property int $id
+ * @property string $name
+ */
 class TestModel extends Model
 {
     use HasSyncTracking;
